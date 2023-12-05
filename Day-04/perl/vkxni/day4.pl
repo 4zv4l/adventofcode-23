fdf9ac5ed6d678d263804f6d663b1abfd2ff5cda
4zv4l <46655455+4zv4l@users.noreply.github.com>
2023-12-05 11:38:56 +0800

    Update day4.pl
    proposing my solution

--- Day-04/perl/vkxni/day4.pl
+++ Day-04/perl/vkxni/day4.pl
@@ -1,80 +1,23 @@
-use strict;
-use warnings;
+#!/usr/bin/perl
 
-open(my $f, '<', "input.txt") or die "Cannot open file: $!";
+while (<>)
+{
+    my (%win, %get, %same);
 
-sub part1 {
-    my @lines; while (my $l = <$f>) { chomp($l); push @lines, $l; }
-    my $sum_part1 = 0;
-    
-    foreach my $i (0 .. $#lines) {
-        my $l = $lines[$i];
-        my @line_split = split(':', $l);
-        my @sets = split(/\|/, $line_split[1]);
-        my $points = 0;
-        my @set0 = split(' ', $sets[0]);
-        my @set1 = grep { $_ =~ /\d+/ } split(' ', $sets[1]);
-    
-    foreach my $n (@set0) {
-        foreach my $x (@set1) {
-            if ($n =~ /\d+/ && $x =~ /\d+/) {
-                if ($n == $x) {
-                    if ($points == 0) {
-                        $points = 1;
-                    } else {
-                        $points *= 2;
-                    }
-                }
-            }
-        }
+    $_ =~ qr{[:]([^|]+)(?{ @win{$^N =~ /(\d+)/g} = () })[|]};
+    $_ =~ qr{[|](.+)(?{ @get{$^N =~ /(\d+)/g} = () })$};
+
+    @same{ grep { exists $get{$_} } keys %win } = ();
+
+    $part1 += int 2**((scalar keys %same)-1);
+
+    my $value = scalar keys %same;
+    for (1..++$copies{$.})
+    {
+        @copies{$.+1..$.+$value} = map {$_+1} @copies{$.+1..$.+$value};
     }
-    $sum_part1 += $points;
-    $points = 0;
-  }
-
-    return $sum_part1;  
+    $part2 += $copies{$.};
 }
 
-sub part2 {
-    # Reset file pointer to the beginning (https://perldoc.perl.org/functions/seek)
-    seek($f, 0, 0);
-
-    my @lines_part2;
-    while (my $l = <$f>) {
-        chomp($l);
-        push @lines_part2, [$l, 1];
-    }
-
-    my $sum_part2 = 0;
-    for (my $i = 0; $i < scalar @lines_part2; $i++) {
-        my $g = $lines_part2[$i];
-        my @l = split(':', $g->[0]);
-        my @sets = split(/\|/, $l[1]);
-        my $points = 0;
-        $sum_part2 += $g->[1];
-    
-        my $count = 0;
-        my @set1 = grep { $_ =~ /\d+/ } split(' ', $sets[1]);
-    
-    foreach my $n (split(' ', $sets[0])) {
-        foreach my $x (@set1) {
-            if ($n =~ /\d+/ && $x =~ /\d+/) {
-                if ($n == $x) {
-                    $count++;
-                }
-            }
-        }
-    }
-
-    for (my $x = 0; $x < $count; $x++) {
-        $lines_part2[$i + 1 + $x]->[1] += $g->[1];
-    }
-    $sum_part2 += $points;
-    $points = 0;
-  }
-
-  return $sum_part2;
-}
-
-print "Part 1: " . part1() . "\n";
-print "Part 2: " . part2() . "\n";
+print "part1: $part1\n";
+print "part2: $part2\n";
